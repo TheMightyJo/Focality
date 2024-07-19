a80416fb9c13b9991b018697e19d4ffe811f76f2
Pat75013 <karukera92@gmail.com>
2024-07-19 15:23:37 +0200

    Ajout Timer Model et TimerViewModel

--- Focality/Models/Timer.swift
+++ Focality/Models/Timer.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+class Timer: Identifiable{
+    var id = UUID()
+    var focusTime:  Int
+    var shortBreak : Int
+    var longBreak : Int
+    
+    init(id: UUID = UUID(), focusTime: Int, shortBreak: Int, longBreak: Int) {
+        self.id = id
+        self.focusTime = focusTime
+        self.shortBreak = shortBreak
+        self.longBreak = longBreak
+    }
+    
+}
