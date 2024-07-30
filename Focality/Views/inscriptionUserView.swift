527a267e41eeebbcdfa500e460324ddc174cc65e
Dembod <diaouned@yahoo.fr>
2024-07-30 10:07:59 +0200

    inscriptionUserView
    creation view

--- Focality/Views/inscriptionUserView.swift
+++ Focality/Views/inscriptionUserView.swift
@@ -7,51 +7,14 @@
 
 import SwiftUI
 
-struct inscriptionUserView : View {
-    @StateObject var userViewModel = UserViewModel()
-    
-//    @ObservedObject  var userViewModel = UserViewModel()
-    
-//    var user = User(firstName: "Dembo", lastName: "Babar", email: "dembo@gmail.com", password: "Teamcook5*", birthday: Date(), point: 55, currentLevel: 150)
-//    var firstName = ""
-//    var lastName = ""
-//    var email = ""
-//    var password = ""
-//    var birthday = Date()
-//    var point = 0
-//    var currentLevel = 0
+struct inscriptionUserView: View {
    
     var body: some View {
-
-        VStack{
-            Text("Inscription")
-                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
-                .font(.system(size: 36))
-            
-            Image("Logo")
-                .resizable()
-                .frame(width: 200, height: 200)
-        }
-        
-        
-        HStack{
-            List{
-//                TextField(userViewModel.users,text: $firstName)
-               
-                
-                
-                
-                
-                
-                    
-                
-            }
-                
-            
-        }
+        Text("Hello, World!")
     }
 }
 
 #Preview {
-    inscriptionUserView(userViewModel: UserViewModel())
+    inscriptionUserView()
 }
+
