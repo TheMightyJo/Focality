b8835cffdb02703144f787c5dc721385444bc9a9
Johan Q. <johan.quille@protojo.fr>
2024-08-05 09:46:40 +0200

    Update OnboardingView.swift

--- Focality/Views/OnboardingView.swift
+++ Focality/Views/OnboardingView.swift
@@ -3,66 +3,95 @@ import SwiftUI
 struct OnboardingView: View {
     @ObservedObject var challengeViewModel: ChallengeViewModel
     @ObservedObject var userViewModel: UserViewModel
+    @StateObject private var viewModel = TimerViewModel(isRunning: false, currentTime: 0) // Instance du ViewModel pour gérer l'état
     @State private var showTimerView = false
     
+    
     var body: some View {
-        VStack {
-            Text("Bienvenue à Focality")
-                .font(.largeTitle)
-                .bold()
-                .padding(.bottom, 20)
-            
-            Text("Voici vos premiers défis :")
-                .font(.title2)
-                .padding(.bottom, 20)
-            
-            if challengeViewModel.challenges.count >= 2 {
-                HStack(spacing: 20) {
-                    ChallengeRowView(userViewModel: userViewModel, challenge: challengeViewModel.challenges[0])
-                        .frame(width: UIScreen.main.bounds.width / 2 - 30, height: 200)
+        NavigationStack {
+            ScrollView {
+                VStack {
+                    HStack {
+                        Image("Logo")
+                            .resizable()
+                            .frame(width: 50, height: 50)
+                        Spacer()
+                        Image("ProfilePicture")
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: 50, height: 50)
+                            .clipShape(Circle())
+                    }
+                    Text("Bienvenue sur Focality")
+                        .font(.largeTitle)
+                        .bold()
+                        .padding(.bottom, 20)
                     
-                    ChallengeRowView(userViewModel: userViewModel, challenge: challengeViewModel.challenges[1])
-                        .frame(width: UIScreen.main.bounds.width / 2 - 30, height: 200)
+                    if challengeViewModel.challenges.count >= 2 {
+                        VStack(spacing: 20) {
+                            ChallengeRowView(userViewModel: userViewModel, challenge: challengeViewModel.challenges[0])
+                            
+                            ChallengeRowView(userViewModel: userViewModel, challenge: challengeViewModel.challenges[1])
+                        }
+                        .padding(.horizontal)
+                    } else {
+                        Text("Pas assez de défis disponibles.")
+                    }
+                    Spacer()
+                    NavigationLink(destination: TimerView()) {
+                        VStack {
+                            ZStack {
+                                Ellipse()
+                                    .fill(Color.primaire)
+                                    .frame(width: 54, height: 54)
+                                    .padding()
+                                Image(systemName: "play.fill")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 26, height: 26)
+                                    .foregroundColor(Color(.black))
+                                    .padding(.leading, 5)
+                            }
+                            Text("1 heure")
+                                .font(.title).bold()
+                        }
+                    }
+                    
+                    Button(action: {
+                        showTimerView.toggle()
+                    }) {
+                        Image(systemName: "fitness.timer.fill")
+                        Text("Lancer le Minuteur")
+                            .font(.headline)
+                            .foregroundColor(.white)
+                            .padding()
+                            .frame(maxWidth: .infinity)
+                            .background(Color.blue)
+                            .cornerRadius(10)
+                            .padding(.horizontal, 20)
+                    }
+                    .padding(.bottom, 20)
+                    .sheet(isPresented: $showTimerView) {
+                        TimerView()
+                    }
+                    
+                    Button(action: {
+                        // Action pour démarrer
+                    }) {
+                        Text("Commencer")
+                            .font(.headline)
+                            .foregroundColor(.white)
+                            .padding()
+                            .frame(maxWidth: .infinity)
+                            .background(Color.blue)
+                            .cornerRadius(10)
+                            .padding(.horizontal, 20)
+                    }
+                    .padding(.bottom, 40)
                 }
-                .padding(.horizontal)
-            } else {
-                Text("Pas assez de défis disponibles.")
+                .padding()
             }
-            
-            Spacer()
-            
-            Button(action: {
-                showTimerView.toggle()
-            }) {
-                Text("Lancer le Minuteur")
-                    .font(.headline)
-                    .foregroundColor(.white)
-                    .padding()
-                    .frame(maxWidth: .infinity)
-                    .background(Color.blue)
-                    .cornerRadius(10)
-                    .padding(.horizontal, 20)
-            }
-            .padding(.bottom, 20)
-            .sheet(isPresented: $showTimerView) {
-                TimerView()
-            }
-            
-            Button(action: {
-                // Action pour démarrer
-            }) {
-                Text("Commencer")
-                    .font(.headline)
-                    .foregroundColor(.white)
-                    .padding()
-                    .frame(maxWidth: .infinity)
-                    .background(Color.blue)
-                    .cornerRadius(10)
-                    .padding(.horizontal, 20)
-            }
-            .padding(.bottom, 40)
         }
-        .padding()
     }
 }
 
