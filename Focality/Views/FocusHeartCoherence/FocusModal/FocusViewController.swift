787df5c86c2667ba2866726bb8abb88234c2d58e
Dembod <diaouned@yahoo.fr>
2024-08-06 17:00:35 +0200

    creation modal UIkit

---
+++ Focality/Views/FocusHeartCoherence/FocusModal/FocusViewController.swift
@@ -0,0 +1,24 @@
+//
+//  FocusViewController.swift
+//  Focality
+//
+//  Created by Apprenant 142 on 06/08/2024.
+//
+
+import UIKit
+
+class FocusViewController : UIViewController{
+    
+    
+    
+    
+    @IBOutlet weak var labelView: UILabel!
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+
