369408370260ec8ad4627748b12964c8d1aa8df3
Dembod <diaouned@yahoo.fr>
2024-08-08 11:16:50 +0200

    DocC sur inscription

--- Focality/Views/Authentification/InscriptionUserView.swift
+++ Focality/Views/Authentification/InscriptionUserView.swift
@@ -1,8 +1,13 @@
 import SwiftUI
 
+///La vue InscriptionUserView permet aux utilisateurs de s'inscrire en entrant leurs informations personnelles.
+/// Elle utilise le modèle de vue UserViewModel pour gérer les données utilisateur.
 struct InscriptionUserView: View {
+/// Le modèle de vue utilisé pour gérer les utilisateurs.
     @ObservedObject var userViewModel: UserViewModel
+/// Le modèle de vue utilisé pour gérer l'authentification
     @ObservedObject var authViewModel: AuthViewModel
+/// Propriétés d'état pour stocker les entrées utilisateur.
     @State private var firstName = ""
     @State private var lastName = ""
     @State private var email = ""
@@ -12,50 +17,52 @@ struct InscriptionUserView: View {
 
     var body: some View {
         VStack {
+/// Titre de l'inscription.
             Text("Inscription")
                 .fontWeight(.bold)
                 .font(.system(size: 30))
                 .padding(.trailing, 200)
-
+/// Logo de l'application.
             Image("Logo")
                 .resizable()
                 .frame(width: 100, height: 100)
 
             List {
+/// Champ de texte pour le prénom.
                 HStack {
                     Text("FirstName")
                     TextField("FirstName", text: $firstName)
                         .padding()
                         .textFieldStyle(RoundedBorderTextFieldStyle())
                 }
-
+/// Champ de texte pour le nom de famille.
                 HStack {
                     Text("LastName")
                     TextField("LasttName", text: $lastName)
                         .padding()
                         .textFieldStyle(RoundedBorderTextFieldStyle())
                 }
-
+/// Champ de texte pour l'email.
                 HStack {
                     Text("Email")
                     TextField("Email", text: $email)
                         .padding()
                         .textFieldStyle(RoundedBorderTextFieldStyle())
                 }
-
+/// Champ de texte pour le mot de passe.
                 HStack {
                     Text("Password")
                     SecureField("Password", text: $password)
                         .padding()
                         .textFieldStyle(RoundedBorderTextFieldStyle())
                 }
-
+/// Sélecteur de date pour la date de naissance.
                 HStack {
                     DatePicker("Birthday", selection: $birthday, displayedComponents: .date)
                         .padding()
                         .textFieldStyle(RoundedBorderTextFieldStyle())
                 }
-
+/// Bouton pour créer un utilisateur.
                 ZStack {
                     RoundedRectangle(cornerRadius: 10)
                         .fill(Color.primaire)
