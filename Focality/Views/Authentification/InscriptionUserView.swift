9aced8f402ead1ff2677afe20f779e8e91c200af
Dembod <diaouned@yahoo.fr>
2024-08-08 16:59:35 +0200

    docc

--- Focality/Views/Authentification/InscriptionUserView.swift
+++ Focality/Views/Authentification/InscriptionUserView.swift
@@ -23,6 +23,7 @@ struct InscriptionUserView: View {
                 .font(.system(size: 30))
                 .padding(.trailing, 200)
 /// Logo de l'application.
+<<<<<<< Updated upstream
             Image("Logo")
                 .resizable()
                 .frame(width: 100, height: 100)
@@ -30,40 +31,90 @@ struct InscriptionUserView: View {
             List {
 /// Champ de texte pour le prénom.
                 HStack {
+=======
+                Image("Logo")
+                    .resizable()
+                    .frame(width: 100, height: 100)
+            List{
+    /// Champ de texte pour le prénom.
+                HStack{
+>>>>>>> Stashed changes
                     Text("FirstName")
                     TextField("FirstName", text: $firstName)
                         .padding()
                         .textFieldStyle(RoundedBorderTextFieldStyle())
                 }
+<<<<<<< Updated upstream
 /// Champ de texte pour le nom de famille.
                 HStack {
+=======
+    /// Champ de texte pour le nom de famille.
+                HStack{
+>>>>>>> Stashed changes
                     Text("LastName")
                     TextField("LasttName", text: $lastName)
                         .padding()
                         .textFieldStyle(RoundedBorderTextFieldStyle())
                 }
+<<<<<<< Updated upstream
 /// Champ de texte pour l'email.
                 HStack {
+=======
+               
+/// Champ de texte pour l'email.
+                HStack{
+>>>>>>> Stashed changes
                     Text("Email")
                     TextField("Email", text: $email)
                         .padding()
                         .textFieldStyle(RoundedBorderTextFieldStyle())
                 }
 /// Champ de texte pour le mot de passe.
+<<<<<<< Updated upstream
                 HStack {
+=======
+                HStack{
+>>>>>>> Stashed changes
                     Text("Password")
                     SecureField("Password", text: $password)
                         .padding()
                         .textFieldStyle(RoundedBorderTextFieldStyle())
                 }
 /// Sélecteur de date pour la date de naissance.
+<<<<<<< Updated upstream
                 HStack {
+=======
+                HStack{
+                    
+>>>>>>> Stashed changes
                     DatePicker("Birthday", selection: $birthday, displayedComponents: .date)
                         .padding()
                         .textFieldStyle(RoundedBorderTextFieldStyle())
                 }
+<<<<<<< Updated upstream
 /// Bouton pour créer un utilisateur.
                 ZStack {
+=======
+                
+//                HStack{
+//                    Text("Points")
+//                    TextField("Points", value: $point, formatter: NumberFormatter())
+//                    .keyboardType(.numberPad)
+//                    .padding()
+//                    .textFieldStyle(RoundedBorderTextFieldStyle())
+//                }
+//               
+//                HStack{
+//                    Text("Current Level")
+//                    TextField("Current Level", value: $currentLevel, formatter: NumberFormatter())
+//                    .keyboardType(.numberPad)
+//                    .padding()
+//                    .textFieldStyle(RoundedBorderTextFieldStyle())
+//                }
+//                
+/// Bouton pour créer un utilisateur.
+                ZStack{
+>>>>>>> Stashed changes
                     RoundedRectangle(cornerRadius: 10)
                         .fill(Color.primaire)
                         .frame(width: 300, height: 50)
