bde7d44927944e293aed873116a2c46023a0234a
Johan Q. <johan.quille@protojo.fr>
2024-08-08 17:04:27 +0200

    Commit

--- Focality/Views/Authentification/InscriptionUserView.swift
+++ Focality/Views/Authentification/InscriptionUserView.swift
@@ -3,22 +3,15 @@ import SwiftUI
 ///La vue InscriptionUserView permet aux utilisateurs de s'inscrire en entrant leurs informations personnelles.
 /// Elle utilise le modèle de vue UserViewModel pour gérer les données utilisateur.
 struct InscriptionUserView: View {
-/// Le modèle de vue utilisé pour gérer les utilisateurs.
+    /// Le modèle de vue utilisé pour gérer les utilisateurs.
     @ObservedObject var userViewModel: UserViewModel
-/// Le modèle de vue utilisé pour gérer l'authentification
+    /// Le modèle de vue utilisé pour gérer l'authentification
     @ObservedObject var authViewModel: AuthViewModel
-<<<<<<< Updated upstream
-/// Propriétés d'état pour stocker les entrées utilisateur.
-    @State private var firstName = ""
-    @State private var lastName = ""
-    @State private var email = ""
-    @State private var password = ""
-=======
+    /// Propriétés d'état pour stocker les entrées utilisateur.
     @State private var firstName = "Emiliano"
     @State private var lastName = "Babar"
     @State private var email = "emiliano@gmail.com"
     @State private var password = "Teamcook5*"
->>>>>>> Stashed changes
     @State private var birthday = Date()
     @State private var selectedImage: UIImage? = UIImage(named: "ProfilePicture") // Image par défaut d'Emiliano
     @State private var isImagePickerPresented = false
@@ -26,18 +19,15 @@ struct InscriptionUserView: View {
     
     var body: some View {
         VStack {
-/// Titre de l'inscription.
+            /// Titre de l'inscription.
             Text("Inscription")
                 .fontWeight(.bold)
                 .font(.system(size: 30))
                 .padding(.trailing, 200)
-<<<<<<< Updated upstream
-/// Logo de l'application.
+            /// Logo de l'application.
             Image("Logo")
                 .resizable()
                 .frame(width: 100, height: 100)
-
-=======
             
             Button(action: {
                 isImagePickerPresented = true
@@ -58,64 +48,42 @@ struct InscriptionUserView: View {
             .sheet(isPresented: $isImagePickerPresented) {
                 ImagePicker(selectedImage: $selectedImage)
             }
-            
->>>>>>> Stashed changes
             List {
-/// Champ de texte pour le prénom.
+                /// Champ de texte pour le prénom.
                 HStack {
                     Text("FirstName")
                     TextField("FirstName", text: $firstName)
                         .padding()
                         .textFieldStyle(RoundedBorderTextFieldStyle())
                 }
-<<<<<<< Updated upstream
-/// Champ de texte pour le nom de famille.
-=======
-                
->>>>>>> Stashed changes
+                /// Champ de texte pour le nom de famille.
                 HStack {
                     Text("LastName")
                     TextField("LastName", text: $lastName)
                         .padding()
                         .textFieldStyle(RoundedBorderTextFieldStyle())
                 }
-<<<<<<< Updated upstream
-/// Champ de texte pour l'email.
-=======
-                
->>>>>>> Stashed changes
+                /// Champ de texte pour l'email.
                 HStack {
                     Text("Email")
                     TextField("Email", text: $email)
                         .padding()
                         .textFieldStyle(RoundedBorderTextFieldStyle())
                 }
-<<<<<<< Updated upstream
-/// Champ de texte pour le mot de passe.
-=======
-                
->>>>>>> Stashed changes
+                /// Champ de texte pour le mot de passe.
                 HStack {
                     Text("Password")
                     SecureField("Password", text: $password)
                         .padding()
                         .textFieldStyle(RoundedBorderTextFieldStyle())
                 }
-<<<<<<< Updated upstream
-/// Sélecteur de date pour la date de naissance.
-=======
-                
->>>>>>> Stashed changes
+                /// Sélecteur de date pour la date de naissance.
                 HStack {
                     DatePicker("Birthday", selection: $birthday, displayedComponents: .date)
                         .padding()
                         .textFieldStyle(RoundedBorderTextFieldStyle())
                 }
-<<<<<<< Updated upstream
-/// Bouton pour créer un utilisateur.
-=======
-                
->>>>>>> Stashed changes
+                /// Bouton pour créer un utilisateur.
                 ZStack {
                     RoundedRectangle(cornerRadius: 10)
                         .fill(Color.primaire)
