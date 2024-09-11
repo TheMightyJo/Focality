73274ae1d7b550e65b55e9683d60197f70de8e84
EmG9999 <emilianomondorge@gmail.Com>
2024-09-11 14:21:47 +0200

    changement anglais au francais

--- Focality/Views/Authentification/InscriptionUserView.swift
+++ Focality/Views/Authentification/InscriptionUserView.swift
@@ -16,91 +16,94 @@ struct InscriptionUserView: View {
     @State private var navigateToContentView = false
     
     var body: some View {
-        VStack {
-            /// Titre de l'inscription.
-            Text("Inscription")
-                .fontWeight(.bold)
-                .font(.system(size: 30))
-                .padding(.trailing, 200)
+        NavigationView {
             
-            /// Logo de l'application.
-            Image("Logo")
-                .resizable()
-                .frame(width: 100, height: 100)
-            
-            List {
-                /// Champ de texte pour le prénom.
-                HStack {
-                    Text("FirstName")
-                    TextField("FirstName", text: $firstName)
-                        .padding()
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                }
+        
+            VStack {
+               
                 
-                /// Champ de texte pour le nom de famille.
-                HStack {
-                    Text("LastName")
-                    TextField("LastName", text: $lastName)
-                        .padding()
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                }
+                /// Logo de l'application.
+                Image("Logo")
+                    .resizable()
+                    .frame(width: 100, height: 100)
                 
-                /// Champ de texte pour l'email.
-                HStack {
-                    Text("Email")
-                    TextField("Email", text: $email)
-                        .padding()
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                }
-                
-                /// Champ de texte pour le mot de passe.
-                HStack {
-                    Text("Password")
-                    SecureField("Password", text: $password)
-                        .padding()
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                }
-                
-                /// Sélecteur de date pour la date de naissance.
-                HStack {
-                    DatePicker("Birthday", selection: $birthday, displayedComponents: .date)
-                        .padding()
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                }
-                
-                /// Bouton pour créer un utilisateur.
-                ZStack {
-                    RoundedRectangle(cornerRadius: 10)
-                        .fill(Color.primaire)
-                        .frame(width: 300, height: 50)
+                List {
+                    /// Champ de texte pour le prénom.
+                    HStack {
+                        Text("Prenom")
+                        TextField("FirstName", text: $firstName)
+                            
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                    }
                     
-                    Button(action: {
-                        /// Mise à jour des informations utilisateur dans le ViewModel d'authentification.
-                        authViewModel.firstName = firstName
-                        authViewModel.lastName = lastName
-                        authViewModel.email = email
-                        authViewModel.password = password
-                        authViewModel.birthday = birthday
-                        authViewModel.signUp()
+                    /// Champ de texte pour le nom de famille.
+                    HStack {
+                        Text("Nom")
+                        TextField("LastName", text: $lastName)
+                            
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                    }
+                    
+                    /// Champ de texte pour l'email.
+                    HStack {
+                        Text("Email")
+                        TextField("Email", text: $email)
+                           
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                    }
+                    
+                    /// Champ de texte pour le mot de passe.
+                    HStack {
+                        Text("Mot de passe")
+                        SecureField("Password", text: $password)
+                           
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                    }
+                    
+                    /// Sélecteur de date pour la date de naissance.
+                    HStack {
+                        DatePicker("Date de naissance", selection: $birthday, displayedComponents: .date)
+                          
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                    }
+                    
+                    /// Bouton pour créer un utilisateur.
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 10)
+                            .fill(Color.primaire)
+                            .frame(width: 300, height: 50)
                         
-                        /// Si l'inscription réussit, navigation vers la vue principale.
-                        if authViewModel.isSignUpSuccess {
-                            navigateToContentView = true
-                        }
-                    }, label: {
-                        Text("Création")
-                    })
-                    .foregroundColor(.black)
-                    .font(.custom("arial", size: 30))
+                        Button(action: {
+                            /// Mise à jour des informations utilisateur dans le ViewModel d'authentification.
+                            authViewModel.firstName = firstName
+                            authViewModel.lastName = lastName
+                            authViewModel.email = email
+                            authViewModel.password = password
+                            authViewModel.birthday = birthday
+                            authViewModel.signUp()
+                            
+                            /// Si l'inscription réussit, navigation vers la vue principale.
+                            if authViewModel.isSignUpSuccess {
+                                navigateToContentView = true
+                            }
+                        }, label: {
+                            Text("Création")
+                        })
+                        .foregroundColor(.black)
+                        .font(.custom("arial", size: 30))
+                    }
+                }.listRowSpacing(30.0)
+              
+                .navigationTitle("Inscription")
+                .scrollContentBackground(.hidden)
+                
+                
+                /// Navigation vers ContentView si l'inscription est réussie.
+                NavigationLink(destination: ContentView(authViewModel: authViewModel)
+                    .navigationBarBackButtonHidden(true),
+                               isActive: $navigateToContentView) {
+                    EmptyView()
                 }
-            }
-            .scrollContentBackground(.hidden)
-            
-            /// Navigation vers ContentView si l'inscription est réussie.
-            NavigationLink(destination: ContentView(authViewModel: authViewModel)
-                           .navigationBarBackButtonHidden(true),
-                           isActive: $navigateToContentView) {
-                EmptyView()
             }
         }
     }
